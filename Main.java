a1b1be08f7dec17b5611a15ca95ae0ad98504027
Lucca Piovezan <70606998+LuccaPiovezan@users.noreply.github.com>
2021-04-29 12:19:06 -0300

    Expressões Lambdas Stream API

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
-package br.cm.softblue.lambdastream;
+package lambda.strem.api;
 
 import java.util.stream.IntStream;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		
 		int mult = 2;
 		
 		IntStream.range(1, 21)
-		.map(i -> i * mult)
-		.forEach(System.out::println);
+			.map(i -> i * mult)
+			.forEach(System.out::println);
+		)
 	}
-
 }
